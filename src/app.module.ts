5bf23ee12bd837e9aa9a39471580c6e0000f3500
aguallogil <aguallogil_oscar@hotmail.com>
2024-08-30 15:22:30 -0700

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MealModule } from './modules/meal/meal.module';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),  // Accede a MONGO_URI usando ConfigService
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    } as MongooseModuleOptions),
+    MealModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
